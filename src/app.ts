dfa7787e03444e0795a9c5355c4bab9e36f50359
pedroobando <pedroobando@hotmail.com>
2018-04-18 21:16:16 -0400

    finalizado Sucursales

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,8 @@ app.use(bodyParser.json({limit: '5mb'}));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Expose-Headers", "x-total-count");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
-  res.header("Access-Control-Allow-Headers", "Content-Type,authorization");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
 
   next();
 });
--- src/routes/sucursal.ts
+++ src/routes/sucursal.ts
@@ -2,18 +2,15 @@ import {Router, Request, Response, NextFunction} from 'express';
 import {Sucursal} from '../models/Sucursal';
 import {Almacen} from '../models/Almacen';
 
-// import {MovieActor} from '../models/MovieActor';
-
 export const sucursals = Router();
 
-sucursals.get('/', async (req: Request, res: Response, next: NextFunction) => {
+sucursals.get('/all', async (req: Request, res: Response, next: NextFunction) => {
   try {
     // res.json();
     res.json({
       status: 200,
       request_url: req.originalUrl,
       message: await Sucursal.scope(req.query['scope']).findAndCountAll()
-      // message: await Sucursal.scope(req.query['scope']).findAll({include: [Almacen]})
     });
     
   } catch (e) {
@@ -22,9 +19,8 @@ sucursals.get('/', async (req: Request, res: Response, next: NextFunction) => {
   }
 });
 
-sucursals.get('/all', async (req: Request, res: Response, next: NextFunction) => {
+sucursals.get('/', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    // res.json();
     const PAGESIZE: number = 30;
     const order: string = !req.query.order ? 'ASC' : req.query.order.toUpperCase();
     const filtername: string = !req.query.filtername ? '' : req.query.filtername.toUpperCase();
@@ -47,86 +43,129 @@ sucursals.get('/all', async (req: Request, res: Response, next: NextFunction) =>
       });
 
     res.status(200).json(sucursales);
-      // message: await Sucursal.scope(req.query['scope']).findAll({include: [Almacen]})
-    
-
-    // res.json({
-    //   status: 200,
-    //   request_url: req.originalUrl,
-    //   menssge: order,
-    //   filternam: filtername,
-    //   message: await Sucursal.scope(req.query['scope']).findAndCountAll({
-    //     limit: limitPage,
-    //     offset: offset2,
-    //     where: { alma_des: {$like: `%${filtername}`}}
-    //   }).then((objectAll) => {
-    //     const totalItems: number = objectAll.count;
-    //     const totalPage: number = Math.ceil(objectAll.count / limitPage);
-    //     const showItem: number = (objectAll.rows.length);
-    //     return _returnJson(200,
-    //       `Pagina ${activePage} de ${totalPage}`,
-    //       _clearObjectAll(objectAll.rows),
-    //       _paginate(activePage, totalPage, totalItems, showItem));
-    //   })
-    //   // message: await Sucursal.scope(req.query['scope']).findAll({include: [Almacen]})
-    // });    
     
   } catch (e) {
     console.log(e);
-    res.status(500).json(_errorObject(e, '/all'));
+    res.status(500).json(_errorObject(e, '/'));
     next(e);
   }
 });
 
-
-
-
-sucursals.get('/:id', async (req: Request, res: Response, next) => {
+sucursals.get('/:keyId', async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const Id: string = req.params.keyId.trim();
+    let numRequest: number = 0;
+    if (!Id) {
+      return res.status(400).json(_clearObject({id: 0, name: '', active: false}));
+    }
 
     const sucursal = await Sucursal.scope(req.query['scope']).findOne({
-      where: {co_alma: req.params.id},
+      where: { co_alma: Id},
       include: [Almacen]
+    }).then((theObject) => {
+      if (theObject) {
+        numRequest = 200;
+        return {data: _clearObject(theObject), rows: _clearAlmacenAll(theObject.almacenes)};  
+      } else {
+        numRequest = 404;
+        return {data: {co_alma: '0', name: ''}};
+      }
+    }).catch((err) => {
+      console.log(err);
+      res.status(500).json(_errorObject(err, '/'));
     });
-    res.json(sucursal);
+    res.status(numRequest).json(sucursal);
   } catch (e) {
     next(e);
   }
 });
 
+sucursals.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const uuidv4 = require('uuid/v4');
+    await Sucursal.create({
+      co_alma: req.body.keyId,
+      alma_des: req.body.name,
+      co_sucu:  req.body.co_sucu,
+      nro_fact: req.body.nro_fact,
+      rowguid: uuidv4()
+    }).then((theObject) => {
+      res.sendStatus(201);
+    }).catch((err) => {
+      // console.log(err);
+      res.status(500).json(_errorObject(err, '/'));
+    });    
+  } catch (e) {
+    next(e);
+  }
+});
 
+sucursals.put('/:keyId', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const Id: string = req.params.keyId.trim();
+    await Sucursal.update(req.body,
+    {
+      where: {
+        co_alma: Id
+      }
+    }).then((affectedRows) => {
+      if (affectedRows["0"]) {
+        res.sendStatus(202);
+      } else {
+        res.sendStatus(404);
+      }
+    }).catch((err) => {
+      res.status(500).json(_errorObject(err, '/'));
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+sucursals.delete('/:keyId', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const Id: string = req.params.keyId.trim();
+    await Sucursal.destroy(
+    {
+      where: {
+        co_alma: Id
+      }
+     }).then((affectedRows) => {
+      if (affectedRows >= 1) {
+        res.sendStatus(202);
+      } else {
+        res.sendStatus(404);
+      }
+    }).catch((err) => {
+      res.status(500).json(_errorObject(err, '/'));
+    });
+  } catch (e) {
+    next(e);
+  }
+});
 
 function _clearObjectAll(_objectAll) {
-  let objectAll = [];
+  const objectAll = [];
   _objectAll.forEach((tObject) => {
     objectAll.push(_clearObject(tObject));
   });
  return objectAll;
 }
 
-// function _clearAlmacenAll(_objectAll) {
-//   var objectAll = [];
-//   _objectAll.forEach((tObject) => {
-//     objectAll.push({co_sub: tObject.co_sub.trim(), des_sub: tObject.des_sub.trim()})
-//   });
-//  return objectAll;
-// }
+function _clearAlmacenAll(_objectAll) {
+  const objectAll = [];
+  _objectAll.forEach((tObject) => {
+    objectAll.push({co_sub: tObject.co_sub.trim(), des_sub: tObject.des_sub.trim()});
+  });
+ return objectAll;
+}
 
 function _clearObject(_object) {
   return {
-    co_alma: _object.co_alma.trim(), name: _object.alma_des.trim()
+    co_alma: _object.co_alma.trim(),
+    name: _object.alma_des.trim()
   };
 }
-
-// function _clearObjectAlmacen(_objectAll)
-// {
-//   let objectAll = [];
-//   _objectAll.forEach((tObject) => {
-//     objectAll.push({co_sub: tObject.co_sub.trim(), des_sub: tObject.des_sub.trim()})
-//    });
-//    return objectAll;
-// }
-
 
 function _errorObject(_err, onfunction) {
   return {
@@ -137,7 +176,6 @@ function _errorObject(_err, onfunction) {
 
 function _returnJson(_data, _thepaginate) {
   return {
-    // statusCode:_statusCode, message:_message, data:{ records:_data, paginate: _thepaginate }
     rows: _data, paginate: _thepaginate,  
   };
 }
